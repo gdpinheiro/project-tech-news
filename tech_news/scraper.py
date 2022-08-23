9c167476ba91a602b23f187ffeb0c1150ce4d351
black <gikigiio@gmail.com>
2022-08-23 16:16:46 -0300

    Req 1

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,16 @@
+import requests
+import time
+
+
 # Requisito 1
 def fetch(url):
     """Seu código deve vir aqui"""
+    time.sleep(1)
+    response = requests.get(url, headers={"user-agent": "Fake user-agent"})
+    if response.status_code == 200:
+        return response.content
+    else:
+        return None
 
 
 # Requisito 2
